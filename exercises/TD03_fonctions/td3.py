05eda8d575a3aba593e5793efbb96b7f8c6cf85f
moaye.komenan <moaye.komenan@ens.uvsq.fr>
2022-10-13 09:58:17 +0200

    modifTD finalisé

---
+++ exercises/TD03_fonctions/td3.py
@@ -0,0 +1,9 @@
+def tempsEnSeconde(jour:int, heure:int, minutes:int, seconde:int):
+    """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
+     input("Saisir un temps")
+
+
+   
+   
+   
+    print(tempsEnSeconde)
